94894f1d42229dafd83fa101d636a1f0047744ee
Prashant Solanki <solankisrp2@gmail.com>
2023-01-03 01:24:11 +0530

    Wait for parallel process to finish before exiting

--- convert-images.sh
+++ convert-images.sh
@@ -13,6 +13,10 @@ filename='assets/scss/_mixins.scss'
 # Create an array to store the captured groups
 declare -a screen_widths
 
+# Create list to stroe process numbers
+declare -a PROCNUMS_CONVERT
+declare -a PROCNUMS_SCALE
+
 # Read the file and store the captured groups in the array
 while IFS= read -r line; do
   if [[ $line =~ max-width:([0-9]+)px ]]; then
@@ -29,29 +33,44 @@ for directory in ${directories[@]}; do
 
   # Iterate over the screen widths
   for screen_width in ${screen_widths[@]}; do
-  
+    ITERATOR=0
     # Use convert to resize the image and save it to a file with a suffix indicating the screen width
     for image_file in ${image_files[@]}; do
       base_name=$(echo "$image_file" | rev | cut -f 2- -d '.' | rev)
       convert "$image_file" -resize "${screen_width}x" "${base_name}_${screen_width}.jpg" &
+      PROCNUMS_SCALE[$ITERATOR]=$!
+      let "ITERATOR=ITERATOR+1"
     done
   done
 
   # Delete Original files
+  ITERATOR=0
   for image_file in ${image_files[@]}; do
+    wait ${PROCNUMS_SCALE[$ITERATOR]}
     rm -f $image_file &
+    let "ITERATOR=ITERATOR+1"
   done
 
+  # Get new list of files after scaling and deleting originals
   image_files=($(find "$directory" -type f -name "*.jpg" -o -name "*.png" -o -name "*.jpeg" -o -name "*.bmp"  -o -name "*.tiff" ))
 
   # Iterate over the array of image files
+  ITERATOR=0
   for image_file in ${image_files[@]}; do
     # Get the base name of the image file (i.e. the file name without the extension)
     base_name=$(echo "$image_file" | rev | cut -f 2- -d '.' | rev)
-    echo $image_file
     # Use cwebp to create a webp version of the image
     cwebp "$image_file" -o "${base_name}.webp" &
-    npx avif --input="$image_file" --output "$directory" &
-    # rm -rf $image_file
+    npx avif --input="$image_file" --output "$directory" --lossless --effort 6 --quality 80 &
+    PROCNUMS_CONVERT[$ITERATOR]=$!
+    let "ITERATOR=ITERATOR+1"
   done
+
+  # Wait for files to be converted
+  ITERATOR=0
+  for image_file in ${image_files[@]}; do
+    wait ${PROCNUMS_CONVERT[$ITERATOR]}
+    let "ITERATOR=ITERATOR+1"
+  done
+
 done
