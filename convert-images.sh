1f0c6bee50ca4c5662e0a917790126d9c62baeaa
Prashant Solanki <solankisrp2@gmail.com>
2023-01-03 00:25:22 +0530

    Scale portfolio images based on max-width specified in _mixins.scss

--- convert-images.sh
+++ convert-images.sh
@@ -7,9 +7,33 @@ directories=("assets/images/portfolio/handmade"
 "assets/images/portfolio/digital"
 "assets/images/portfolio/accessories")
 
+# Set the filename
+filename='assets/scss/_mixins.scss'
+
+# Create an array to store the captured groups
+declare -a screen_widths
+
+# Read the file and store the captured groups in the array
+while IFS= read -r line; do
+  if [[ $line =~ max-width:(\d+)px ]]; then
+    screen_widths+=( ${BASH_REMATCH[1]} )
+  fi
+done < "$filename"
+
+echo "Captured screen widths: ${screen_widths[@]}"
+
 # Iterate over the directories
 for directory in ${directories[@]}; do
   # Find all image files in the directory and store the results in an array
+  image_files=($(find "$directory" -type f -name "*.jpg" -o -name "*.png" -o -name "*.jpeg" -o -name "*.bmp"  -o -name "*.tiff" ))
+
+  # Iterate over the screen widths
+  for screen_width in ${screen_widths[@]}; do
+    # Use convert to resize the image and save it to a file with a suffix indicating the screen width
+    convert "$image_file" -resize "${screen_width}x" "${base_name}_${screen_width}.jpg"
+    rm -f $image_file
+  done
+
   image_files=($(find "$directory" -type f -name "*.jpg" -o -name "*.png" -o -name "*.jpeg" -o -name "*.bmp"  -o -name "*.tiff" ))
 
   # Iterate over the array of image files
